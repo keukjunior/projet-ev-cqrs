6f49eb17aa8864d43fa719676daafb5971b19c2c
keukjunior <mapsjunior@hotmail.cm>
2026-04-28 14:41:29 +0000

    firt commit

---
+++ src/main/java/sn/maps/projetevcqrs/ProjetEvCqrsApplication.java
@@ -0,0 +1,13 @@
+package sn.maps.projetevcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEvCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEvCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/maps/projetevcqrs/ProjetEvCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.maps.projetevcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEvCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
